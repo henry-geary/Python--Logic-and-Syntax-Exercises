4c429430acf6dfe118ec196d6b61fd90d59d7ba1
elHenryettas <francisco.geary@mercadolibre.com>
2023-04-26 16:46:47 -0300

    refactor

--- app/paddocks/paddock_exercises/exercise_2.py
+++ app/paddocks/paddock_exercises/exercise_2.py
@@ -1,6 +1,8 @@
+from .paddock_db import paddockType as PAD_TYPES
 
 
-# 2 Arreglo con los nombres de cada tipo de cultivo, ordenados decrecientemente por la suma TOTAL de la cantidad de hectáreas plantadas de cada uno de ellos.
+# 2 Arreglo con los nombres de cada tipo de cultivo, ordenados decrecientemente por la suma TOTAL de la cantidad de
+# hectáreas plantadas de cada uno de ellos.
 
 def two():
-    return "hello"
+    return PAD_TYPES
